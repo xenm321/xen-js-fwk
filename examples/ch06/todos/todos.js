73ed2720d4a678f646ca99678d190dd7900d4793
Alexandr Medvedev <al.medvedev@kryptonite.ru>
2024-05-28 16:36:20 +0300

    create examples

--- examples/ch06/todos/todos.js
+++ examples/ch06/todos/todos.js
@@ -6,10 +6,20 @@ createApp({
   reducers: {
     add: (state, amount) => state + amount,
   },
-  view: (state, emit) =>
-    h(
-      'button',
-      { on: { click: () => emit('add', 1) } },
-      [hString(state)]
-    ),
+  view: (state, emit) => {
+    return h('div', {}, [
+      h(
+        'button',
+        { on: { click: () => emit('add', 1) } },
+        [hString(state)]
+      ),
+      h('br'),
+      h('br'),
+      h(
+        'p',
+        {},
+        [hString(`Result is ${state}`)]
+      )
+    ])
+  },
 }).mount(document.body)
